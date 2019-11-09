6eeb1e22653ab6a88d50f3202df5857babb8a730
Ryan <ryanz@princeton.edu>
2019-11-09 14:37:27 -0500

    continue writing RNN code

--- backend/LSTM.py
+++ backend/LSTM.py
@@ -1,17 +1,36 @@
 import pandas as pd
 import os
-
-def make_train_dict(inputPath):
-	df = pd.read_csv(inputPath)
-	packedData = df['Body']
-	print(len(packedData))
+import collections
+import re
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import LSTM, Conv2D, Embedding, Dense
+from keras.models import Model, Sequential
+from keras.optimizers import Adam
 
 def load_data(inputDir):
-	# Find training data
 	trainingPath = os.path.join(inputDir, "train.csv")
-	validPath = os.path.join(inputDir, "valid.csv")
-	testPath = os.path.join(inputDir, "test.csv")
+	df = pd.read_csv(trainingPath)
+	data = df.dropna()
+	data['Body'] = data['Body'].apply((lambda x: re.sub('[^a-zA-z0-9\s]','',x)))
+	labels = data['Label']
+	
+	tokenizer = Tokenizer(nb_words = 50000, lower = True, split = ' ')
+	tokenizer.fit_on_texts(data['Body'].values)
+	train_data = tokenizer.texts_to_sequences(data['Body'])
+	train_data = pad_sequences(train_data)
+	return train_data, labels
 
-	make_train_dict(trainingPath)
+def build_model(train_data):
+	model = Sequential()
+	model.add(Embedding(50000, 128, input_length = train_data.shape[1]))
+	model.add(LSTM(100, dropout=0.2))
+	model.add(Dense(1, activation = 'sigmoid'))
+	return model
 
-load_data("./train_data/")
+train_data, labels = load_data("./train_data/")
+print(labels)
+model = build_model(train_data)
+optimizer = Adam(.01, .5)
+model.compile(optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+model.fit(train_data, labels, batch_size = 32, epochs = 10)
