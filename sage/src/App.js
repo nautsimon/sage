f528c4a47b8cdd0009f8c7854f73979ed556c7c8
Simon mahns <ak.simonm@gmail.com>
2019-11-09 10:40:33 -0500

    update

--- sage/src/App.js
+++ sage/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* global chrome */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import icon from "./imgs/actIco.png";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      description: "",
+      link: ""
+    };
+  }
+
+  componentDidMount() {
+    let getInfo = function() {
+      return new Promise(function(resolve, reject) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(
+          tabs
+        ) {
+          chrome.tabs.sendMessage(tabs[0].id, { from: "reactapp" }, function(
+            response
+          ) {
+            var data = {
+              title: response.title,
+              description: response.description,
+              link: response.link
+            };
+            resolve(data);
+          });
+        });
+      });
+    };
+    getInfo().then(data => {
+      return this.setState({
+        title: data.title,
+        description: data.description,
+        link: data.link
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="modalBody">
+        <div className="row">
+          <div className="left">
+            <img className="icon" src={icon} />
+          </div>
+          <div className="right">
+            <p className="titleText">
+              Act Now:{" "}
+              <a className="titleLink" href={this.state.link} target="_blank">
+                {this.state.title}
+              </a>
+            </p>
+            <p className="genText">{this.state.description}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
